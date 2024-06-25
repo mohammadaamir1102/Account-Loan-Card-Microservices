40c1340211fc75cc35e4395f0e00840faaf95a05
mohammadaamir1102 <er.aamir1102@gmail.com>
2024-06-25 22:21:34 +0530

    audit enabling like updateAt, update By etc

--- Account/src/main/java/com/aamir/account/AccountApplication.java
+++ Account/src/main/java/com/aamir/account/AccountApplication.java
@@ -2,8 +2,10 @@ package com.aamir.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl") // this is for enabling the BaseEntity auditing scenarios like createAt, createdBy etc.
 public class AccountApplication {
 
 	public static void main(String[] args) {
