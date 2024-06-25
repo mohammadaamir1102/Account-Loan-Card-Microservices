c278abf30565d3bfabc7685a62f51ef772f8ff82
mohammadaamir1102 <er.aamir1102@gmail.com>
2024-06-25 22:55:24 +0530

    documentation added of swagger

--- Account/src/main/java/com/aamir/account/AccountApplication.java
+++ Account/src/main/java/com/aamir/account/AccountApplication.java
@@ -1,11 +1,36 @@
 package com.aamir.account;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl") // this is for enabling the BaseEntity auditing scenarios like createAt, createdBy etc.
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Khan Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Aamir Khan",
+						email = "aamirtech1102@gmail.com",
+						url = "https://github.com/mohammadaamir1102"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/mohammadaamir1102"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Khan Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui.html"
+		)
+)
 public class AccountApplication {
 
 	public static void main(String[] args) {
--- Account/src/main/java/com/aamir/account/controller/AccountsController.java
+++ Account/src/main/java/com/aamir/account/controller/AccountsController.java
@@ -2,8 +2,15 @@ package com.aamir.account.controller;
 
 import com.aamir.account.constant.AccountsConstants;
 import com.aamir.account.dto.CustomerDto;
+import com.aamir.account.dto.ErrorResponseDto;
 import com.aamir.account.dto.ResponseDto;
 import com.aamir.account.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.slf4j.Logger;
@@ -15,6 +22,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in Khan Bank",
+        description = "CRUD REST APIs in Khan Bank to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(path="/account/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
@@ -23,6 +34,25 @@ public class AccountsController {
     @Autowired
     private IAccountsService iAccountsService;
     private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
+
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer &  Account inside KhanBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
@@ -30,6 +60,25 @@ public class AccountsController {
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
+
+    @Operation(
+            summary = "Fetch Account Details REST API",
+            description = "REST API to fetch Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
 
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(
@@ -53,6 +102,28 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account & Customer Details REST API",
+            description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(
             @RequestParam @Pattern(regexp = "(^$|[0-9]{10})",
